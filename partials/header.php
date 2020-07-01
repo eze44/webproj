2bf879d682bcdffe7ea410975e874d41163a752d
zhuri <zhuriendrit@gmail.com>
2020-07-01 11:36:59 +0200

    header

--- partials/header.php
+++ partials/header.php
@@ -7,19 +7,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap"
-		rel="stylesheet">
-
 	<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-
 	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- <link rel="stylesheet" href="css/owl.theme.default.min.css"> -->
 
-	<link rel="stylesheet" href="css/aos.css">
+	<!-- <link rel="stylesheet" href="css/aos.css"> -->
 	<link rel="stylesheet" href="css/flaticon.css">
-	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-		integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/styled.css">
-	<link rel="stylesheet" href="css/about-style.css">
 </head>
