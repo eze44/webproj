1a50f644403afbd09ff7ed654f6f5e2f5395e4ed
zhuri <zhuriendrit@gmail.com>
2020-07-01 14:35:40 +0200

    updating the lorem ipsums

--- about.php
+++ about.php
@@ -14,185 +14,57 @@
 								id="defaultOpen">About Us</button>
 
 								<button class="tablinks" onclick="openCity(event, 'London')"
-								id="defaultOpen">Project2Process</button>
+								id="defaultOpen">Something else</button>
 
-								<button class="tablinks" onclick="openCity(event, 'Paris')">About the ALLED2 project</button>
+								<button class="tablinks" onclick="openCity(event, 'Paris')">Test1</button>
 
-								<button class="tablinks" onclick="openCity(event, 'Tokyo')">Project Boards</button>
+								<button class="tablinks" onclick="openCity(event, 'Tokyo')">Test2</button>
 
 						</div>
 						<div id="Contenttabs">
 						<div id="generalAboutUs" class="tabcontent">
-							<p>This is official page of the project “Aligning Education and Training with Labour Market Needs – ALLED Phase II”, financed by european union and the austrian development cooperation. As presented and approved in the project Inception report for communication purposes acronym ALLED2 will be used in the same understanding.</p>
-							<p>ALLED 2 project is a continuation of <a href="http://www.alledkosovo.com/">ALLED 1</a></p>
-
-							<p>For societies sustainable development, being Industry 4.0 ready, able to compete in global market with every citizen included, quality of education and training and lifelong learning is essential for economic growth and development and wellbeing of every citizen.</p>
 							<strong class="text-primary">People are the most valuable resource</strong>
-							<p>For each society being able to compete in global market and being Industry 4.0 ready aligning education and training with labour market needs is a must as a constant process for smart planning and development in the long-term. Let’s move step forward towards the process through ALLED2 project as the future is in our schools today.</p>
+							<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
-							<p>Being aware that education is a backbone of every society ALLED2 project is building further of the achievements of the ALLED1 with clear objective to move from project driven to process embedded developments in Kosovo in these sphere being aware that world of work and world of education are two hands of the same body.</p>
+</p>
+
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 						</div>
 
 						<div id="London" class="tabcontent">
-							<h3>Project2Process</h3>
-							<p>
-								The project is financed by EU within financial envelop IPA II 2017 for Kosovo with
-								co-funding by the Austrian Development Cooperation ADA. According to the IPA 2017
-								financing decision project is implemented by Austrian Development Agency (ADA)
-								represented by Austrian Development Cooperation Office in Kosovo via Indirect Management
-								(IM).</p>
-							<p>Read more about the project </p>
-							<p>Project is implemented by ADA Project Implementation Unite (PIU) team.</p>
+							<h3>Process</h3>
+							<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
-							<p>Principles of structured knowledge management related to knowledge cumulation, knowledge
-								sharing and peer learning as well as transfer of tacit knowledge to formalized knowledge
-								will be applied horizontally. </p>
+</p>
 
-							<p>Through the whole project implementation environment protection awareness, gender
-								mainstreaming and social inclusion/impact principles will be horizontally applied in all
-								project results and activities. </p>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 						</div>
 
 						<div id="Paris" class="tabcontent">
-							<h3>About the ALLED2 project </h3>
-							<p>The overall objective of the project ALLED2 is to reduce poverty through increased labour
-								market participation and improved employability for the population in Kosovo.</p>
+							<h3>About the project </h3>
+							<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
-							<p>All planned actions and activities will support Kosovo’s Ministry of Education, Science
-								and Technology (MEST) and Ministry of Labour and Social Welfare (MLSW) in reforming the
-								education and training system to link education and training with labour market needs,
-								and to reflect the European Framework of Key Competences.</p>
+</p>
 
-							<p>The specific purpose of the ALLED2 is to strengthen the quality and relevance of
-								education and training programmes for the labour market and to support the adaptation of
-								the legislative framework and mechanisms as a precondition for increased employability.
-							</p>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
-							<p>Attention is on emphasizing the further step in aligning education with the labour
-								market; in building, concrete cooperation models and measures between education and
-								training and private sector with systemic effects. The new structured approach of
-								building an education system responsive to the labour market and the many opportunities
-								bringing both education and labour market representatives together, highlight innovation
-								as a driving force in aligning education with labour market needs. </p>
+							<p><strong class="text-primary">Testing</strong>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
-							<p>ALLED2 project aims to support innovative structured approach based on triple helix
-								principle (structured public sector – academia – business cooperation) building an
-								education system, especially VET, responsive to the labor market needs and contributing
-								to economic growth and development and overal employability. </p>
-							<p> The specific objectives of the project, in line with the “EU Action Document for Kosovo
-								2017”, Kosovo Education Strategy and Kosovo Economic Reform Agenda, are: </p>
+	</p>
 
-							<p>1. Strengthen the link between Higher Education and Labour Market Needs by the
-								development and modernisation of HE programmes according to labour market needs; </p>
-
-							<p>2. Improve the teacher training programmes in order for them to conform with the
-								requirements of the New Curriculum of Kosovo and the priorities of MEST;</p>
-
-							<p>3. Enhance the quality of practical and applied teaching and learning in Vocational
-								Education and Training (VET) schools and Vocational Training Centres (VTCs) in core
-								occupational sectors relevant to market needs, including career guidance and
-								counselling; </p>
-
-							<p>4. Enable a better understanding of beneficiary and international qualifications and
-								further develop and modernise Kosovo’s Qualification System for Qualification Framework
-								professional qualifications.</p>
-
-							<p>ALLED 2 project will support upgrading of VET programs as well as quality of teaching and
-								learning processes in following sectors in Kosovo: agriculture, food processing,
-								mechanical engineering and energy sector. Sectors of project interventions are selected
-								based on recommendations and findings from “Labour Market Needs Analysis – Perspective
-								for the future in Kosovo” (July 2019, ALLED2).</p>
-
-							<p>Through the whole project implementation environment protection awareness, gender
-								mainstreaming and social inclusion/impact principles will be horizontally applied in all
-								project results and activities.</p>
-
-							<p>The project are contributing to the following <strong class="text-primary">expected
-									results:</strong></p>
-
-							<p>Expected Result 1: Study programmes serve as a competence hub for the industry and VET:
-								curricula, diploma supplement and faculty management are enhanced, labour
-								market-relevant and lead to increased employability;</p>
-
-							<p>Expected Result 2: Professional qualifications and occupational standards meet NQF
-								criteria for validation, Quality Assurance (QA) arrangements are further improved and
-								are elaborated at the policy level and the link to progression pathways between HE and
-								VET is defined;</p>
-
-							<p>Expected Result 3: Vocational Education Training (VET) schools and Vocational Training
-								Centres (VTCs) offer qualitative programmes based on the Labour Market Needs; including
-								Training of Teachers/Trainers (ToT), sustainable quality assurance mechanisms and
-								provision of relevant equipment; </p>
-
-							<p>Expected Result 4: Education and training system related Governance procedures support
-								the alignment of education and training with labour market needs;</p>
-
-							<p>Expected Result 5: The link between the Vocational Education and Training (VET) and the
-								Business sector is more structured; certain planning tools for VET are sustainably in
-								place; and incentives are identified and piloted.</p>
-
-							<p><strong class="text-primary">Project Direct beneficiaries:</strong> Ministry of
-								Education, Science and Technology (MEST), Ministry of Labour and Social Welfare (MLSW),
-								Public universities, Municipal Education Departments (MED), VET schools, Vocational
-								Training Centres (VTC), Kosovo Accreditation Agency (KAA), National Qualifications
-								Authority (NQA). </p>
-
-							<p><strong class="text-primary">Project Indirect beneficiaries:</strong> HE, VET and VTC
-								students, job seekers, private sector, adult learners, including diverse women and men.
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 							</p>
 						</div>
 
 						<div id="Tokyo" class="tabcontent">
 
 							<h3>Steering Board</h3>
-							<p>As defined per DoA the project is steered by Steering Board. In the inception phase
-								Steering Board ToR1 have been developed and approved (by EU delegation). Project SB will
-								provide support for the project’s implementation, adequate monitoring of the project’s
-								progress, overall strategic guidance of all activities performed during project
-								implementation and receiving and commenting reporting. The Steering Board will meet on a
-								regular basis (at least twice a year)2. Additional meetings of the Steering Board can be
-								held on request from the members of the Steering Board. The Steering Board will be
-								informed by, and reported to, by the PIU about the progress of the project, and will
-								make recommendations based on the information provided by the PIU and stakeholders’
-								comments. The Steering Board evaluate, discuss and endorse all required reports
-								(Inception, semi-annual, annual and final). The PIU will incorporate all the
-								recommendations given by the Steering Board in its project implementation. </p>
-							<p>The SB consists of one representative of each of the following institutions: </p>
-							<p> Ministry of Education Science and Technology (MEST) - voting member </p>
-							<p> Ministry of Labour and Social Welfare (MLSW) - voting member</p>
-							<p> European Union Office in Kosovo (EUOK) - voting member</p>
-							<p> Austrian Development Agency (ADA) - voting member; in the case of equal unresolved voting (equal distribution of votes) ADA has the right of final decision being that it has the overall responsibility for the project</p>
-							<p> European Training Foundation (ETF) - observer - non-voting member</p>
-							<p>Team leader of ALLED 2  - non-voting member </p>
-							<p>In the reporting period respective SB members are nominated by respective institutions as
-								per official communication by PIU in line with ToR. </p>
+							<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
+				</p>
 
-							<h3>Advisory Board</h3>
-							<p> In order to ensure efficient and effective communication as well as sound implementation
-								of the project and involvement of all project stakeholders on the initiative of the PIU,
-								ALLED2 Advisory Board has been be established. </p>
-
-							<p>The Advisory Board (AB) consists of assigned representatives from key project relevant
-								institutions with non-voting right. </p>
-
-							<p>The AB consist of one representative of each of the following institutions: </p>
-							<p>1. Kosovo Accreditation Agency (KAA) </p>
-
-							<p>2. National Qualification Authority (NQA) </p>
-
-							<p>3. Employment Agency of the Republic of Kosovo (EARK) </p>
-
-							<p>4. Rectors Conference (RC) </p>
-
-							<p>5. Kosovo Chamber of Commerce (KCC) </p>
-
-							<p>6. Project Implementation Unit (PIU) – team leader and key component experts </p>
-
-							<p>The AB members may be invited to attend SB meeting and/or present certain topics to SB
-								members (with non-voting right) upon prior announcement. </p>
-
-							<p>Representatives of CO-PRN and EUOK may attend the AB meetings as well.</p>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 						</div>
 
 					<!-- </div> -->
